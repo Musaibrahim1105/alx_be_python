7ab0c22820795cbfd9a63370ada5ee434f77916d
Musa Ibrahim <musaibrahim1105@gmail.com>
2025-09-11 10:22:04 +0100

    the area of the rectangle is calculated and modified

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,5 +2,5 @@ P = principal = 1000
 R = rate = 0.05
 T = time = 3
 I = P * R * T
-interest = I = principal * rate * time
+interest = principal * rate * time
 print("The simple interest is: ", interest)
