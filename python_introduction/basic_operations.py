d34280ec936322371a2efeac534744ade8fd51e3
Musa Ibrahim <musaibrahim1105@gmail.com>
2025-09-11 09:50:52 +0100

    intro to python arithmetic

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+print(" Addition of 10 and 5 is ", sum)
+
+sub = number1 - number2
+print(" Substraction of 10 and 5 is", sub)
+
+mult = number1 * number2
+print(" Multiplication of 10 and 5 is ", mult)
